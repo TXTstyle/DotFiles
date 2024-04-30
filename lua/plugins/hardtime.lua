6b4a78506ff3a0685f92fd7c0ede0519d9da874d
Oskar <oskar.fv57@gmail.com>
2024-04-30 22:50:40 +0200

    notification no focus, hardtime tweak

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -2,16 +2,42 @@ return {
     "m4xshen/hardtime.nvim",
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
     opts = {
-        max_count = 4,
+        disabled_filetypes = {
+            "NvimTree",
+            "TelescopePrompt",
+            "aerial",
+            "alpha",
+            "checkhealth",
+            "dapui*",
+            "Diffview*",
+            "Dressing*",
+            "help",
+            "httpResult",
+            "lazy",
+            "Neogit*",
+            "mason",
+            "neotest-summary",
+            "minifiles",
+            "neo-tree*",
+            "netrw",
+            "noice",
+            "notify",
+            "prompt",
+            "qf",
+            "fugitive",
+            "undotree",
+            "Trouble",
+        },
+        max_count = 5,
         hints = {
             ["k%^"] = {
                 message = function()
                     return "Use - instead of k^" -- return the hint message you want to display
                 end,
-                length = 2,           -- the length of actual key strokes that matches this pattern
+                length = 2,                      -- the length of actual key strokes that matches this pattern
             },
-            ["d[tTfF].i"] = {         -- this matches d + {t/T/f/F} + {any character} + i
-                message = function(keys) -- keys is a string of key strokes that matches the pattern
+            ["d[tTfF].i"] = {                    -- this matches d + {t/T/f/F} + {any character} + i
+                message = function(keys)         -- keys is a string of key strokes that matches the pattern
                     return "Use " .. "c" .. keys:sub(2, 3) .. " instead of " .. keys
                     -- example: Use ct( instead of dt(i
                 end,
